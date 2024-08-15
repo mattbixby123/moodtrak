a0e26796001dc8ee1dc5421c56632dd2f8c90430
mattbixby123 <matthew.bixby1@gmail.com>
2024-08-15 17:37:26 -0400

    first commit

---
+++ app/dashboard/page.js
@@ -0,0 +1,29 @@
+import Dashboard from '@/components/Dashboard';
+import Login from '@/components/Login';
+import Main from '@/components/Main';
+import React from 'react'
+export const metadata = {
+  title: "moodtrak ⋅ Dashbaord",
+};
+
+const DashboardPage = () => {
+
+
+  const isAuthenticated = false
+  let children = (
+    <Login />
+  )
+
+  if (isAuthenticated) {
+    children = (
+      <Dashboard />
+    )
+  }
+  return (
+    <Main>
+      {children}
+    </Main>
+  )
+}
+
+export default DashboardPage
---
+++ components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Main(props) {
+  const { children } = props
+  return (
+    <main>
+      {children}
+    </main>
+  )
+}
