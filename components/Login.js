4fddaf233e8933b40118c92cd75a2348f9c5812d
mattbixby123 <matthew.bixby1@gmail.com>
2024-08-16 15:57:53 -0400

    log in/register form component complete

--- components/Login.js
+++ components/Login.js
@@ -1,13 +1,21 @@
 import { Fugaz_One } from 'next/font/google';
 import React from 'react'
+import Button from './Button';
 
 const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] });
 
 
 export default function Login() {
   return (
-    <div className='flex flex-col flex-1 justify-center items-center grap-4'>
-      <h3 className={' ' + fugaz.className}>Log In / Register</h3>
+    <div className='flex flex-col flex-1 justify-center items-center gap-4'>
+      <h3 className={'text-4xl sm:text-5xl md:text-6xl ' + fugaz.className}>Log In / Register</h3>
+      <p>You&#39;re one step away!</p>
+      <input className='w-full max-w-[400px] mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border border-solid border-indigo-400 rounded-full outline-none' placeholder='Email' type="text" />
+      <input className='w-full max-w-[400px] mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border border-solid border-indigo-400 rounded-full outline-none' placeholder='password' type="password" />
+      <div className='max-w-[400px] w-full mx-auto'>
+        <Button text="Submit" full />
+      </div>
+      <p className='text-center'>Don&#39;t have an account? <span className='text-indigo-600'>Sign up</span></p>
     </div>
   )
 }
