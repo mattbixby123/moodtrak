148944cb2d7584e563ec6e0ec1899bc0a71bc9f8
mattbixby123 <matthew.bixby1@gmail.com>
2024-08-17 17:40:29 -0400

    committing early stage  progress on AuthContext.js

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,2 +1,26 @@
 'use client'
 
+import React, { useContext, useState, useEffect } from 'react';
+
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null)
+  const [useDataObj, setUserDataOjb] = useState({})
+  const [loading, setLoading] = useState(true);
+
+  const value = {
+
+  }
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
